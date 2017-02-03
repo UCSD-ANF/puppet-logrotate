c61c8ff6b1306888b5cd0a4e16063e77b51748e3
Geoff Davis <gadavis@ucsd.edu>
2017-02-03 12:48:56 -0800

    pin spec to ANF Puppet version, test FreeBSD

--- Gemfile
+++ Gemfile
@@ -1,10 +1,38 @@
-source :rubygems
+source 'https://rubygems.org'
 
-puppetversion = ENV.key?('PUPPET_VERSION') ? "= #{ENV['PUPPET_VERSION']}" : ['>= 3.0']
+### Environment variable version overrrides
 
-gem 'rake'
+# facter
+facter_version = ENV.key?('FACTER_VERSION') ? "= #{ENV['FACTER_VERSION']}" : \
+  '= 2.4.4' # from puppet enterprise 3.8.5
+# puppet
+puppet_version = ENV.key?('PUPPET_VERSION') ? "= #{ENV['PUPPET_VERSION']}" : \
+  '= 3.8.5' # from puppet enterprise 3.8.0, but 3.8.0 not available
+# hiera
+hiera_version = ENV.key?('HIERA_VERSION') ? "= #{ENV['HIERA_VERSION']}" : \
+  '= 1.3.4' # from puppet enterprise 3.8.0
+# hiera-puppet
+hiera_puppet_version = ENV.key?('HIERA_PUPPET_VERSION') ? \
+  "= #{ENV['HIERA_PUPPET_VERSION']}" : '>= 1.0.0' # from puppet enterprise 3.7
+
+### Gem requirements
+gem 'json_pure', '< 2.0'
+gem 'rake', '<11'
 gem 'rspec', '< 3.0.0'
-gem 'rspec-puppet', '>= 1.0.1'
-gem 'puppet', puppetversion
+gem 'facter', facter_version
+gem 'puppet', puppet_version
+gem 'rspec-puppet', '>= 2.0'
+gem 'puppet-syntax', :require => false
+
 gem 'puppet-lint'
+# http://www.camptocamp.com/en/actualite/getting-code-ready-puppet-4/
+gem 'puppet-lint-unquoted_string-check'
+gem 'puppet-lint-empty_string-check'
+gem 'puppet-lint-leading_zero-check'
+gem 'puppet-lint-variable_contains_upcase'
+gem 'puppet-lint-spaceship_operator_without_tag-check'
+#gem 'puppet-lint-absolute_classname-check'
+#gem 'puppet-lint-undef_in_function-check'
+gem 'puppet-lint-roles_and_profiles-check'
+
 gem 'puppetlabs_spec_helper'
--- spec/classes/base_spec.rb
+++ spec/classes/base_spec.rb
@@ -1,91 +1,108 @@
 require 'spec_helper'
 
 describe 'logrotate::base' do
-  it do
-    should contain_package('logrotate').with_ensure('latest')
 
-    should contain_file('/etc/logrotate.conf').with({
+  extend RSpec::SharedContext
+
+  shared_examples_for 'Supported Platform' do
+    it { should contain_package('logrotate').with_ensure('latest') }
+  end
+
+  shared_examples_for 'Linux' do
+    it behaves_like 'Supported Platform'
+    it { should contain_file('/etc/logrotate.conf').with({
       'ensure'  => 'file',
       'owner'   => 'root',
       'group'   => 'root',
       'mode'    => '0444',
       'source'  => 'puppet:///modules/logrotate/etc/logrotate.conf',
       'require' => 'Package[logrotate]',
-    })
+    }) }
 
-    should contain_file('/etc/logrotate.d').with({
+    it { should contain_file('/etc/logrotate.d').with({
       'ensure'  => 'directory',
       'owner'   => 'root',
       'group'   => 'root',
       'mode'    => '0755',
       'require' => 'Package[logrotate]',
-    })
+    }) }
+
+    it { should contain_file('/etc/cron.daily/logrotate').with({
+      'ensure'  => 'file',
+      'owner'   => 'root',
+      'group'   => 'root',
+      'mode'    => '0555',
+      'content' => '/usr/sbin/logrotate /etc/logrotate.conf',
+      'require' => 'Package[logrotate]',
+    }) }
 
   end
 
+
   context 'on Debian' do
-    let(:facts) { {:operatingsystem => 'Debian'} }
-    let(:facts) { {:osfamily => 'Debian'} }
+    let(:facts) { {
+      :operatingsystem => 'Debian',
+      :osfamily => 'Debian',
+    } }
 
-    it do
-      should contain_class('logrotate::defaults::debian')
+    it behaves_like 'Linux'
+    it { should contain_class('logrotate::defaults::debian') }
 
-      should_not contain_cron('logrotate')
-
-      should contain_file('/etc/cron.daily/logrotate').with({
-        'ensure'  => 'file',
-        'owner'   => 'root',
-        'group'   => 'root',
-        'mode'    => '0555',
-        'source'  => 'puppet:///modules/logrotate/etc/cron.daily/logrotate',
-        'require' => 'Package[logrotate]',
-      })
-    end
+    it { should_not contain_cron('logrotate') }
 
   end
 
   context 'on RedHat' do
-    let(:facts) { {:operatingsystem => 'RedHat'} }
-    let(:facts) { {:osfamily => 'RedHat'} }
+    let(:facts) { {
+      :operatingsystem => 'RedHat',
+      :osfamily => 'RedHat',
+    } }
 
-    it do
-      should_not contain_class('logrotate::defaults::debian')
+    it behaves_like 'Linux'
+    it { should_not contain_class('logrotate::defaults::debian') }
 
-      should_not contain_cron('logrotate')
+    it { should_not contain_cron('logrotate') }
 
-      should contain_file('/etc/cron.daily/logrotate').with({
-        'ensure'  => 'file',
-        'owner'   => 'root',
-        'group'   => 'root',
-        'mode'    => '0555',
-        'source'  => 'puppet:///modules/logrotate/etc/cron.daily/logrotate',
-        'require' => 'Package[logrotate]',
-      })
+  end
 
-    end
+  context 'on FreeBSD' do
+    let(:facts) { {
+      :operatingsystem => 'FreeBSD',
+      :osfamily        => 'FreeBSD',
+    } }
+
+    it behaves_like 'Supported Platform'
+
+    it { should contain_file('/etc/cron.daily/logrotate').with({
+      :ensure   => 'file',
+      :owner    => 'root',
+      :group    => 'wheel',
+      :mode     => '0555',
+      :content  => '/usr/local/sbin/logrotate /usr/local/etc/logrotate.conf',
+      :require  => 'Package[logrotate]',
+    }) }
 
   end
 
   context 'on Solaris' do
-    let(:facts) { {:operatingsystem => 'Solaris'} }
-    let(:facts) { {:osfamily => 'Solaris'} }
+    let(:facts) { {
+      :operatingsystem => 'Solaris',
+      :osfamily        => 'Solaris',
+    } }
 
-    it do
-      should contain_package('logrotate').with_provider('pkgutil')
-      should_not contain_class('logrotate::defaults::debian')
+    it behaves_like 'Supported Platform'
+    it { should contain_package('logrotate').with_provider('pkgutil') }
+    it { should_not contain_class('logrotate::defaults::debian') }
 
-      should contain_cron( 'logrotate').with({
-        'command' => '/opt/csw/sbin/logrotate /etc/logrotate.conf',
-        'user'    => 'root',
-        'hour'    => 0,
-        'minute'  => 0,
-        'require' => 'Package[logrotate]',
-      })
+    it { should contain_cron( 'logrotate').with({
+      'command' => '/opt/csw/sbin/logrotate /etc/logrotate.conf',
+      'user'    => 'root',
+      'hour'    => 0,
+      'minute'  => 0,
+      'require' => 'Package[logrotate]',
+    }) }
 
-      should_not contain_file('/etc/cron.daily/logrotate')
-
-    end
-
+    it { should_not contain_file('/etc/cron.daily/logrotate') }
   end
 
 end
